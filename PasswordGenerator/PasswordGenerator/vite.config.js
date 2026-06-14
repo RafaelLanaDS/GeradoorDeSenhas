2829740860e604f63e298ae9827e9030008b3f9d
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-14 11:48:07 -0300

    commit

--- PasswordGenerator/PasswordGenerator/vite.config.js
+++ PasswordGenerator/PasswordGenerator/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GeradoorDeSenhas/', 
+  base: '/GeradoorDeSenhas/', // ← nome EXATO do seu repositório!
 })
